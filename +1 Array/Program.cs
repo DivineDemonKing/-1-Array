3a2a5b0fdd6f8bfa8896180cbb01455a48802744
Георгий <@DESKTOP-IV0D95M>
2022-11-01 13:01:18 +0300

    Добавьте файлы проекта.

---
+++ +1 Array/Program.cs
@@ -0,0 +1,41 @@
+﻿int[] ints = new int[] {1,0,0,0,0};
+bool NotNinesOnly = false;
+bool outOfRange = false;
+int j = 1;
+
+for (int i = 0; i < ints.Length; i++)
+{
+    if (ints[i] < 0 || ints[i] > 9)
+    {
+        outOfRange = true;
+    }
+    if (ints[i] != 9)
+    {
+        NotNinesOnly = true;
+    }
+}
+if (outOfRange)
+{
+    Console.WriteLine("invalid value");
+}
+else if (NotNinesOnly)
+{
+    while (ints[ints.Length - j] == 9)
+    {
+        ints[ints.Length - j] = 0; j++;
+    }
+    ints[ints.Length - j]++;
+    foreach (int intss in ints)
+    {
+        Console.Write(intss);
+    }
+}
+else
+{
+    int[] ints1 = new int[ints.Length + 1];
+    ints1[0] = 1;
+    foreach (int intss in ints1)
+    {
+        Console.Write(intss);
+    }
+}
